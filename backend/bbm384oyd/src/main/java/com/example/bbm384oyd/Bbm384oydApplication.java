9dedce4251026938e136668990f5d8e65e99f669
Acymtoth <hasan258225@yandex.com>
2023-03-22 23:22:41 +0300

    first setup
    first steps of mobil and backend projects

---
+++ backend/bbm384oyd/src/main/java/com/example/bbm384oyd/Bbm384oydApplication.java
@@ -0,0 +1,13 @@
+package com.example.bbm384oyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bbm384oydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bbm384oydApplication.class, args);
+	}
+
+}
---
+++ backend/bbm384oyd/src/test/java/com/example/bbm384oyd/Bbm384oydApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bbm384oyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bbm384oydApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

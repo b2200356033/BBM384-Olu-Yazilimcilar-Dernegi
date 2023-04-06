0bc1d71321d0887cc363e01ebcd52748d5818bbe
Acymtoth <hasan258225@yandex.com>
2023-04-06 18:08:12 +0300

    fix
    fix

--- mobil/app/src/main/java/com/example/oyd/Activities/DepartmentManagerProfilePage.kt
+++ mobil/app/src/main/java/com/example/oyd/Activities/DepartmentManagerProfilePage.kt
@@ -6,21 +6,21 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.oyd.R
-import com.example.oyd.databinding.ActivityAdminProfilePageBinding
+import com.example.oyd.databinding.ActivityDepartmentManagerProfilePageBinding
 
 class DepartmentManagerProfilePage : AppCompatActivity() {
-    private lateinit var binding: ActivityAdminProfilePageBinding
+    private lateinit var binding: ActivityDepartmentManagerProfilePageBinding
     private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAdminProfilePageBinding.inflate(layoutInflater)
+        binding = ActivityDepartmentManagerProfilePageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Set up the navigation drawer
-        val drawerLayout = binding.adminDrawerLayout
-        toggle  = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
-        binding.adminDrawerLayout.addDrawerListener(toggle)
+        val drawerLayout = binding.departmentManagerDrawerLayout
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        binding.departmentManagerDrawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
@@ -31,52 +31,52 @@ class DepartmentManagerProfilePage : AppCompatActivity() {
                     // Handle profile click
                     Toast.makeText(applicationContext,"Clicked Profile",Toast.LENGTH_SHORT).show()
                 }
-                R.id.nav_manage_passwords -> {
-                    // Handle manage passwords click
-                    Toast.makeText(applicationContext,"Clicked Manage Passwords",Toast.LENGTH_SHORT).show()
+                R.id.nav_manage_courses -> {
+                    // Handle manage courses click
+                    Toast.makeText(applicationContext,"Clicked Manage Courses",Toast.LENGTH_SHORT).show()
                 }
-                R.id.nav_manage_roles -> {
-                    // Handle manage roles click
-                    Toast.makeText(applicationContext,"Clicked Manage Roles",Toast.LENGTH_SHORT).show()
+                R.id.nav_view_survey_results -> {
+                    // Handle view survey results click
+                    Toast.makeText(applicationContext,"Clicked View Survey Results",Toast.LENGTH_SHORT).show()
                 }
-                R.id.nav_manage_emails -> {
-                    // Handle manage emails click
-                    Toast.makeText(applicationContext,"Clicked Manage Emails",Toast.LENGTH_SHORT).show()
+                R.id.nav_evaluate_surveys -> {
+                    // Handle evaluate surveys click
+                    Toast.makeText(applicationContext,"Clicked Evaluate Surveys",Toast.LENGTH_SHORT).show()
                 }
-                R.id.nav_add_courses -> {
-                    // Handle add courses click
-                    Toast.makeText(applicationContext,"Clicked Add Courses",Toast.LENGTH_SHORT).show()
+                R.id.nav_manage_instructors -> {
+                    // Handle manage instructors click
+                    Toast.makeText(applicationContext,"Clicked Manage Instructors",Toast.LENGTH_SHORT).show()
                 }
-                R.id.nav_start_semester -> {
-                    // Handle start semester click
-                    Toast.makeText(applicationContext,"Clicked Start Semester",Toast.LENGTH_SHORT).show()
+                R.id.nav_view_eval_results -> {
+                    // Handle view evaluation results click
+                    Toast.makeText(applicationContext,"Clicked View Evaluation Results",Toast.LENGTH_SHORT).show()
                 }
-                R.id.nav_manage_eval_forms -> {
-                    // Handle manage evaluation forms click
-                    Toast.makeText(applicationContext,"Clicked Manage Evaluation Forms",Toast.LENGTH_SHORT).show()
+                R.id.nav_re_evaluate_surveys -> {
+                    // Handle re-evaluate surveys click
+                    Toast.makeText(applicationContext,"Clicked Re-Evaluate Surveys",Toast.LENGTH_SHORT).show()
                 }
-                R.id.nav_check_eval_forms -> {
-                    // Handle check evaluation forms click
-                    Toast.makeText(applicationContext,"Clicked Check Evaluation Forms",Toast.LENGTH_SHORT).show()
+                R.id.nav_share_results -> {
+                    // Handle share results with instructors click
+                    Toast.makeText(applicationContext,"Clicked Share Results with Instructors",Toast.LENGTH_SHORT).show()
                 }
-                R.id.nav_add_user -> {
-                    // Handle add user click
-                    Toast.makeText(applicationContext,"Clicked Add User",Toast.LENGTH_SHORT).show()
+                R.id.nav_upload_resources -> {
+                    // Handle upload resources click
+                    Toast.makeText(applicationContext,"Clicked Upload Resources",Toast.LENGTH_SHORT).show()
                 }
-                R.id.nav_delete_user -> {
-                    // Handle delete user click
-                    Toast.makeText(applicationContext,"Clicked Delete User",Toast.LENGTH_SHORT).show()
+                R.id.nav_download_resources -> {
+                    // Handle download resources click
+                    Toast.makeText(applicationContext,"Clicked Download Resources",Toast.LENGTH_SHORT).show()
                 }
-                R.id.nav_ban_user -> {
-                    // Handle ban user click
-                    Toast.makeText(applicationContext,"Clicked Ban User",Toast.LENGTH_SHORT).show()
+                R.id.nav_share_newsletter -> {
+                    // Handle share newsletter click
+                    Toast.makeText(applicationContext,"Clicked Share Newsletter",Toast.LENGTH_SHORT).show()
                 }
                 R.id.nav_logout -> {
                     // Handle logout click
                     Toast.makeText(applicationContext,"Clicked Logout",Toast.LENGTH_SHORT).show()
                 }
             }
-            binding.adminDrawerLayout.closeDrawers()
+            binding.departmentManagerDrawerLayout.closeDrawers()
             true
         }
     }

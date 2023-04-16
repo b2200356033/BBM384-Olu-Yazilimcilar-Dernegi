216b129e3cf9e0dc6cae115bf50b0a57309e3f00
b2200356033 <b2200356033@cs.hacettepe.edu.tr>
2023-04-16 18:25:51 +0300

    Added Models and Controllers

---
+++ backend/bbm384oyd/src/main/java/com/example/bbm384oyd/Model/Admin.java
@@ -0,0 +1,9 @@
+package com.example.bbm384oyd.Model;
+
+public class Admin {
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String photo;
+}
---
+++ backend/bbm384oyd/src/main/java/com/example/bbm384oyd/Model/DepartmentManager.java
@@ -0,0 +1,10 @@
+package com.example.bbm384oyd.Model;
+
+public class DepartmentManager {
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String photo;
+
+}
